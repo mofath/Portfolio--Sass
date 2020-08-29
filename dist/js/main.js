eb019cf8fcb8a0f146e54603ce6909552f343007
Mhmd Fathi <58465052+mofath@users.noreply.github.com>
2020-08-29 04:50:44 +0200

    adding contact page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,32 +1,80 @@
-// select DOM items
-const menuBtn = document.querySelector('.menu-btn');
+
+// ****************************** start toggle theme script ****************************//
+
+// selectors
+const bodyEl = document.querySelector('body');
+const toggleIcons = document.querySelector('.toggle-icon');
+const modeToggler = document.querySelector(".mode-toggler");
+
+//  initial state 
+let modeToggled = false;
+let isDarkMode = false;
+let darkMode = localStorage.getItem('darkMode');
+
+const sunrise = () => {
+  toggleIcons.children[0].style.opacity = "1";
+  toggleIcons.children[1].style.opacity = "0";
+  toggleIcons.children[1].style.transform = "translateX(-80px) rotate(-360deg)"
+}
+const sunset = () => {
+  toggleIcons.children[0].style.opacity = "0";
+  toggleIcons.children[1].style.opacity = "1";
+  toggleIcons.children[1].style.transform = "translateX(80px) rotate(360deg)"
+}
+
+const enableDarkMode = async () => {
+  bodyEl.classList.remove('light-theme');
+  bodyEl.classList.add('dark-theme');
+  await localStorage.setItem('darkMode', 'enabled');
+  sunset();
+}
+const disableDarkMode = async () => {
+  bodyEl.classList.add('light-theme');
+  bodyEl.classList.remove('dark-theme');
+  await localStorage.setItem('darkMode', null);
+  sunrise();
+}
+
+// sellf invoked function to check if user enabled dark-mode before
+(async () => {
+  if (darkMode === 'enabled') {
+    await enableDarkMode();
+    isDarkMode = true;
+  }
+})();
+
+modeToggler.addEventListener('change', async (event) => {
+  modeToggled = event.target.checked;
+  if (isDarkMode) !modeToggled ? await enableDarkMode() : await disableDarkMode();
+  else modeToggled ? await enableDarkMode() : await disableDarkMode();
+})
+
+// ****************************** end toggle theme script  ****************************//
+
+
+
+// ****************************** start toggle menu script ****************************//
+
+// selectors
+const menuToggler = document.querySelector('.menu-toggler');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
 
-
-// set initial state of menu
+// initial state 
 let showMenu = false;
 
-
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    if (!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
-        showMenu = true;
-    }
-    else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-        showMenu = false;
-    }
+const toggleMenu = () => {
+  if (!showMenu) {
+    menuToggler.classList.add('close');
+    menu.classList.add('show');
+    showMenu = true;
+  }
+  else {
+    menuToggler.classList.remove('close');
+    menu.classList.remove('show');
+    showMenu = false;
+  }
 }
+
+menuToggler.addEventListener('click', toggleMenu);
+
+// ****************************** end toggle menu script ****************************//
